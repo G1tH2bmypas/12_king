abacd6b94e521b42847403204cace015ee67d06c
Tekyto <Tekyto2014@gmail.com>
2022-10-18 14:06:52 +0300

    king

--- index.cpp
+++ index.cpp
@@ -3,5 +3,5 @@
 #include <cmath>
 
 bool king(int x1, int y1, int x2, int y2) {
-	return x2 == x1 && y2 == y1 - 1 || x2 == x1 && y2 == y1 + 1 || y2 == y1 && x2 == x1 - 1 || y2 == y1 && x2 == x1 + 1 || x2 == x1 + 1 && y2 == y1 - 1 || x2 == x1 - 1 && y2 == y1 - 1 || x2 == x1 - 1 && y2 == y1 + 1 || x2 == x1 + 1 && y2 == y1 + 1;
+	return abs(x2 - x1) == 1 && abs(y2 - y1) == 1;
 }
